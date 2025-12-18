d1d741cb7beb7f63bdcfbd24871e77e2740efae1
Daniel Waltz <danielbwaltz@gmail.com>
2025-12-17 23:12:17 -0500

    feat: add linux support

--- Formula/pass-cli.rb
+++ Formula/pass-cli.rb
@@ -24,6 +24,26 @@ class PassCli < Formula
     end
   end
 
+  on_linux do
+    on_intel do
+      url "https://proton.me/download/pass-cli/1.3.1/pass-cli-linux-x86_64"
+      sha256 "22f916541e5d35758660fae357c939e3719d5b63f9e82ab0666b6affe1a2fc13"
+
+      def install
+        bin.install "pass-cli-linux-x86_64" => "pass-cli"
+      end
+    end
+
+    on_arm do
+      url "https://proton.me/download/pass-cli/1.3.1/pass-cli-linux-aarch64"
+      sha256 "20012e31c5af260eaa16cc7610c94200a8b34e5f68d559837530f1a1e51dfb13"
+
+      def install
+        bin.install "pass-cli-linux-aarch64" => "pass-cli"
+      end
+    end
+  end
+
   def caveats
     <<~EOS
       The Proton Pass CLI has been installed as 'pass-cli'.
