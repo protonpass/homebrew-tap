415ed10aac1587a8b623bd465a72f9db5d5413e9
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-02-24 05:37:42 +0600

    chore: update for v1.5.2

--- Casks/proton-pass-cli.rb
+++ Casks/proton-pass-cli.rb
@@ -2,11 +2,11 @@ cask "proton-pass-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "1.4.3"
-  sha256 arm:          "8f546f59f17ef396a6eb8d75d159d7972ab9d9a3dcfc19e53d3c35a3f0907a95",
-         intel:        "0899b82e847a1f7ef38d6f8be9cd16f9ac2478e85fe49361cd1ecc7c0c591fd2",
-         arm64_linux:  "445de4dc53eadd32fb6e98d3c639d050de8f7bc097accf89e8d4d8efc8c13b7a",
-         x86_64_linux: "ac3308ec4c7cc9ce70dc72b2441e0a8cddf17dea147c5b4108bc8fbd2afeba08"
+  version "1.5.2"
+  sha256 arm:          "f24eacb50155502f2f9b2b5857fcd4251b09c554679b67857aa97b1d37c2b2e7",
+         intel:        "cfb2c07f0fc9d8fecd4b2508af4bb0f20ee9ca54a4174123517d4c173b899889",
+         arm64_linux:  "ac88e2ebf15a4799c508408c0cf81dd9180313fe45b67941567ebd30c4fbadb2",
+         x86_64_linux: "b6e02ac79cee277767023dda21b6cea276d56fdb0bf85d96eaf022ff6227debc"
 
   url "https://proton.me/download/pass-cli/#{version}/pass-cli-#{os}-#{arch}",
       verified: "proton.me"
