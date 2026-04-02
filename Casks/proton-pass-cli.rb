39e57c74fe830ad5332dbd43f7f9c01df9d28bb3
Tahmid Khan <60913202+ta7mid@users.noreply.github.com>
2026-04-02 18:27:20 +0600

    update to v1.9.0

--- Casks/proton-pass-cli.rb
+++ Casks/proton-pass-cli.rb
@@ -2,11 +2,11 @@ cask "proton-pass-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "1.8.0"
-  sha256 arm:          "ea4158edc2cdff109ecf869ea6b5781e594fc714c40a25768c8e3be62cecac8f",
-         intel:        "7fc3f43efe035093e5705e2a3e6aa52c41c57d16242ebc663c881a9141586659",
-         arm64_linux:  "b7703238b17499787d782c413a1ddef163c13e8c2ff69599d9641309397e4ad0",
-         x86_64_linux: "33bcd6c556071e333cebf9772bed0047c41c7a2c9d3f49f4b178fdad272d69e2"
+  version "1.9.0"
+  sha256 arm:          "98b62dc3c38dd81f915175640f5d15c88d37777cab770145cfc8220dbfcf88d8",
+         intel:        "0bd9d652291f6827eba15c33e02f4af5d34c5293fa91ba47bae1cc453721e5a5",
+         arm64_linux:  "760ee3345af3b8879728ddf05779e3f7669cc0fd65a84dd01ecba2a2c8ebbcbc",
+         x86_64_linux: "9b71e5957f580266af55b900c89e61ebd402707da2e14c7ea75eded3f546a2f4"
 
   url "https://proton.me/download/pass-cli/#{version}/pass-cli-#{os}-#{arch}",
       verified: "proton.me/"
