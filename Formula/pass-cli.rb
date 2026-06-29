2873dc21e1c380873344e19d118ae1551d71b148
Proton Pass CI <id@proton.ch>
2026-06-29 06:59:14 +0000

    Update pass-cli Formula to version 2.2.1

--- Formula/pass-cli.rb
+++ Formula/pass-cli.rb
@@ -1,13 +1,13 @@
 class PassCli < Formula
   desc "Proton Pass CLI - Command-line interface for Proton Pass"
   homepage "https://proton.me/pass"
-  version "2.2.0"
+  version "2.2.1"
   license "GPL-3.0"
 
   on_macos do
     on_intel do
-      url "https://proton.me/download/pass-cli/2.2.0/pass-cli-macos-x86_64"
-      sha256 "47dc9e7dc62e41013ad48b4e9d1057820e038d8e3b53a98aa73da40d2a489662"
+      url "https://proton.me/download/pass-cli/2.2.1/pass-cli-macos-x86_64"
+      sha256 "f65a64a61e90baea7ba256d9b14720168f0440f5b3d630d16d27df35b455479a"
 
       def install
         bin.install "pass-cli-macos-x86_64" => "pass-cli"
@@ -15,8 +15,8 @@ class PassCli < Formula
     end
 
     on_arm do
-      url "https://proton.me/download/pass-cli/2.2.0/pass-cli-macos-aarch64"
-      sha256 "32deb67778b27ec6cbadf67069f6e60202dcad8f97b14bcd151d81a0f063c69a"
+      url "https://proton.me/download/pass-cli/2.2.1/pass-cli-macos-aarch64"
+      sha256 "fafd72f20c45cb816793f48d8623ce6eb9d06478431923767d36776fde8b450e"
 
       def install
         bin.install "pass-cli-macos-aarch64" => "pass-cli"
@@ -26,8 +26,8 @@ class PassCli < Formula
 
   on_linux do
     on_intel do
-      url "https://proton.me/download/pass-cli/2.2.0/pass-cli-linux-x86_64"
-      sha256 "b880b43bb618c6e3711a41f0652078ab6030205452b988d8f075b71d49445735"
+      url "https://proton.me/download/pass-cli/2.2.1/pass-cli-linux-x86_64"
+      sha256 "f916cf0d584503468e4cecb412b78657b55453822984173fcb9656a3d1a78ce6"
 
       def install
         bin.install "pass-cli-linux-x86_64" => "pass-cli"
@@ -35,8 +35,8 @@ class PassCli < Formula
     end
 
     on_arm do
-      url "https://proton.me/download/pass-cli/2.2.0/pass-cli-linux-aarch64"
-      sha256 "ec4082d8df4c9e2455c1faf8018fba7fc90d8572217377851d9596e1918f634b"
+      url "https://proton.me/download/pass-cli/2.2.1/pass-cli-linux-aarch64"
+      sha256 "5bfa922b289e61333d708c75c77a9dbd7d056f004c838d823bc948acdfb1a5ad"
 
       def install
         bin.install "pass-cli-linux-aarch64" => "pass-cli"
